8f5ab2f18ee545d6606cf8f5192216e6b6667ab0
xavier <3667154287@qq.com>
2025-07-03 10:47:12 +0800

    REPL

--- src/main.c
+++ src/main.c
@@ -6,17 +6,21 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
-  printf("$ ");
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin); // fgets will read a '\n' into the string
-  // if the input is not empty
-  int len = strlen(input);
-  if (len != 0) {
-    input[len - 1] = '\0'; // delete the last '\n'
-    printf("%s: command not found\n", input);
+  while (1) {
+    // Uncomment this block to pass the first stage
+    // printf("$ ");
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    // fgets will read a '\n' into the string
+    // if the input is not empty
+    int len = strlen(input);
+    if (len != 0) {
+      input[len - 1] = '\0'; // delete the last '\n'
+      printf("%s: command not found\n", input);
+    }
+    memset(input, 0, sizeof(input));
   }
   return 0;
 }
