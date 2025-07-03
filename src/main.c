957e9ca41c0c7750b920be7942a45363b05f5950
xavier <3667154287@qq.com>
2025-07-03 11:02:48 +0800

    builtin exit

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  while (1) {
+  while (1)
+  {
     // Uncomment this block to pass the first stage
     // printf("$ ");
     printf("$ ");
@@ -16,11 +18,15 @@ int main(int argc, char *argv[]) {
     // fgets will read a '\n' into the string
     // if the input is not empty
     int len = strlen(input);
-    if (len != 0) {
+    if (len != 0)
+    {
       input[len - 1] = '\0'; // delete the last '\n'
+      if (!strcmp(input, "exit 0")) {
+        break;
+      }
       printf("%s: command not found\n", input);
     }
-    memset(input, 0, sizeof(input));
+    // memset(input, 0, sizeof(input));
   }
   return 0;
 }
